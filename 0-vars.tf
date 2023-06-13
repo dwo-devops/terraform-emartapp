01d8d048f73c611dfa75f52a7abd898c726af1f6
unknown <dwo.transact@gmail.com>
2023-06-13 19:29:00 +0800

    tf files

---
+++ 0-vars.tf
@@ -0,0 +1,15 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "cluster_name" {
+  default = "my-eks"
+}
